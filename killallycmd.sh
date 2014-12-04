942ae8b488c5bf71bc4e61d9c8223f25c2fbbdae
houzy <h-zy2008@hotmail.com>
2014-12-04 17:58:52 +0800

    Update killallycmd.sh

--- killallycmd.sh
+++ killallycmd.sh
@@ -2,6 +2,12 @@
 
 me=`basename $0`
 
-ps -ef | grep ycmd | grep -v `basename $0` | grep -v 'grep'
+if [ x"$1" = x"show" ]; then
+    ps -ef | grep ycmd | grep -v `basename $0` | grep -v 'grep'
+elif [ x"$1" = x"kill" ]; then
+    ps -ef | grep ycmd | grep -v `basename $0` | grep -v 'grep'
+    ps -ef | grep ycmd | grep -v `basename $0` | grep -v 'grep' | awk '{print $2;}' | xargs kill
+else
+    echo "Input show or kill"
+fi
 
-ps -ef | grep ycmd | grep -v `basename $0` | grep -v 'grep' | awk '{print $2;}' | xargs kill
